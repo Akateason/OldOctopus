0e79c80ef7a171eb83e64559f39f4e944bcba3aa
谢天宸 <xietianchen@shimo.im>
2019-02-19 15:51:30 +0800

    Initial Commit

---
+++ Notebook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notebook
+//
+//  Created by teason23 on 2019/2/19.
+//  Copyright © 2019 teason23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotebookTests/NotebookTests.m
@@ -0,0 +1,37 @@
+//
+//  NotebookTests.m
+//  NotebookTests
+//
+//  Created by teason23 on 2019/2/19.
+//  Copyright © 2019 teason23. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NotebookTests : XCTestCase
+
+@end
+
+@implementation NotebookTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
