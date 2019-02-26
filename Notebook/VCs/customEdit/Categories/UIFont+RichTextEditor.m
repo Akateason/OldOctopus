cb428952d467b59e13b6f14ed7ea5a70df68e101
谢天宸 <xietianchen@shimo.im>
2019-02-26 17:31:04 +0800

    [debug] coretext font italic !

--- Notebook/VCs/customEdit/Categories/UIFont+RichTextEditor.m
+++ Notebook/VCs/customEdit/Categories/UIFont+RichTextEditor.m
@@ -1,29 +1,10 @@
 //
 //  UIFont+RichTextEditor.m
-//  RichTextEdtor
+//  Notebook
 //
-//  Created by Aryan Gh on 7/21/13.
-//  Copyright (c) 2013 Aryan Ghassemi. All rights reserved.
-//
-// https://github.com/aryaxt/iOS-Rich-Text-Editor
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+//  Created by teason23 on 2019/2/21.
+//  Copyright © 2019 teason23. All rights reserved.
+
 
 #import "UIFont+RichTextEditor.h"
 
@@ -36,48 +17,21 @@
 }
 
 + (UIFont *)fontWithName:(NSString *)name size:(CGFloat)size boldTrait:(BOOL)isBold italicTrait:(BOOL)isItalic {
-    NSMutableDictionary *tmpDic = [@{ UIFontDescriptorFamilyAttribute : name } mutableCopy];
-
-    //    UIFont *oldApiFont = [self oldfontWithName:name size:size boldTrait:isBold] ;
-    //    UIFontDescriptor *fontDescriptor = oldApiFont.fontDescriptor ;
-
-    UIFontDescriptor *fontDescriptor = [UIFontDescriptor fontDescriptorWithFontAttributes:tmpDic];
-
-    if (isBold) {
-        //        fontDescriptor = [fontDescriptor fontDescriptorWithSymbolicTraits:UIFontDescriptorTraitBold] ;
-
-        //        fontDescriptor = [fontDescriptor fontDescriptorWithFace:@"Bold"] ;
-
-        //        NSDictionary *fontTraitsDictionary = @{UIFontWeightTrait : @(1.0)};
-        //        [tmpDic setObject:fontTraitsDictionary forKey:UIFontDescriptorTraitsAttribute] ;
-
-        [tmpDic setObject:@{ UIFontWeightTrait : @.4 } forKey:UIFontDescriptorTraitsAttribute];
-    }
-    else {
-        [tmpDic setObject:@{ UIFontWeightTrait : @0 } forKey:UIFontDescriptorTraitsAttribute];
-    }
-
-    if (isItalic) {
-        NSValue *matrix = [NSValue valueWithCGAffineTransform:CGAffineTransformIdentity];
-        matrix          = [NSValue valueWithCGAffineTransform:CGAffineTransformMake(1, 0, tanf(15 * (CGFloat)M_PI / 180), 1, 0, 0)];
-        [tmpDic setObject:matrix forKey:UIFontDescriptorMatrixAttribute];
-    }
-
-    fontDescriptor = [fontDescriptor fontDescriptorByAddingAttributes:tmpDic];
-    UIFont *font   = [UIFont fontWithDescriptor:fontDescriptor size:size];
-    return font;
-}
-
-// core text old api
-+ (UIFont *)oldfontWithName:(NSString *)name size:(CGFloat)size boldTrait:(BOOL)isBold {
     NSString *postScriptName = [UIFont postscriptNameFromFullName:name];
 
     CTFontSymbolicTraits traits = 0;
     CTFontRef newFontRef;
     CTFontRef fontWithoutTrait = CTFontCreateWithName((__bridge CFStringRef)(postScriptName), size, NULL);
 
-    if (isBold)
+    CGAffineTransform matrix = CGAffineTransformIdentity;
+    if (isItalic) {
+        //        traits |= kCTFontItalicTrait;
+        matrix = CGAffineTransformMake(1, 0, tanf(15 * (CGFloat)M_PI / 180), 1, 0, 0);
+    }
+
+    if (isBold) {
         traits |= kCTFontBoldTrait;
+    }
 
     if (traits == 0) {
         newFontRef = CTFontCreateCopyWithAttributes(fontWithoutTrait, 0.0, NULL, NULL);
@@ -86,12 +40,16 @@
         newFontRef = CTFontCreateCopyWithSymbolicTraits(fontWithoutTrait, 0.0, NULL, traits, traits);
     }
 
+    CFRelease(fontWithoutTrait);
+
     if (newFontRef) {
-        NSString *fontNameKey = (__bridge NSString *)(CTFontCopyName(newFontRef, kCTFontPostScriptNameKey));
-        UIFont *font          = [UIFont fontWithName:fontNameKey size:CTFontGetSize(newFontRef)];
+        NSString *fontNameKey  = (__bridge NSString *)(CTFontCopyName(newFontRef, kCTFontPostScriptNameKey));
+        UIFontDescriptor *desc = [UIFontDescriptor fontDescriptorWithName:fontNameKey matrix:matrix];
+        UIFont *resFont        = [UIFont fontWithDescriptor:desc size:CTFontGetSize(newFontRef)];
         CFRelease(newFontRef);
-        return font;
+        return resFont;
     }
+
     return nil;
 }
 
@@ -108,9 +66,6 @@
 }
 
 - (BOOL)isBold {
-    //    UIFontDescriptor *descroptor = self.fontDescriptor;
-    //    BOOL isbold = (descroptor.symbolicTraits & UIFontDescriptorTraitBold) != 0;
-    //    return isbold ;
     CTFontRef ctFont            = CTFontCreateWithName((__bridge CFStringRef)self.fontName, self.pointSize, NULL);
     CTFontSymbolicTraits traits = CTFontGetSymbolicTraits(ctFont);
     BOOL isBold                 = ((traits & kCTFontBoldTrait) == kCTFontBoldTrait);
