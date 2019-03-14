ddf79c2eb3cc6c8842a7f2e05bff905af4dfa7be
谢天宸 <xietianchen@shimo.im>
2019-03-14 16:01:19 +0800

    [dev] title style done

--- Notebook/Model/markdownEditor/Module/model/MDHeadModel.m
+++ Notebook/Model/markdownEditor/Module/model/MDHeadModel.m
@@ -29,17 +29,59 @@
     return str ;
 }
 
+- (NSString *)prefixOfTitle {
+    return [[self.str componentsSeparatedByString:@" "] firstObject] ;
+}
+
 - (NSMutableAttributedString *)addAttrOnPreviewState:(NSMutableAttributedString *)attributedString
                                               config:(MDThemeConfiguration *)configuration {
 
     NSDictionary *resultDic = configuration.basicStyle ;
 //    UIFont *paragraphFont = configuration.font ;
     NSUInteger location = self.range.location ;
-    NSUInteger length = self.range.length ;
+//    NSUInteger length = self.range.length ;
     
     switch (self.type) {
         case MarkdownSyntaxHeaders: {
-            NSString *prefix = [[self.str componentsSeparatedByString:@" "] firstObject] ;
+            NSString *prefix = [self prefixOfTitle] ;
+            NSUInteger numberOfmark = prefix.length ;
+            switch (numberOfmark) {
+                case 1: resultDic = @{NSFontAttributeName : [UIFont boldSystemFontOfSize:32]}; break;
+                case 2: resultDic = @{NSFontAttributeName : [UIFont boldSystemFontOfSize:24]}; break;
+                case 3: resultDic = @{NSFontAttributeName : [UIFont boldSystemFontOfSize:20]}; break;
+                case 4: resultDic = @{NSFontAttributeName : [UIFont boldSystemFontOfSize:16]}; break;
+                case 5: resultDic = @{NSFontAttributeName : [UIFont boldSystemFontOfSize:14]}; break;
+                case 6: resultDic = @{NSFontAttributeName : [UIFont boldSystemFontOfSize:12]}; break;
+                default: break;
+            }
+            [attributedString addAttributes:resultDic range:self.range] ;
+            
+            // hide "# " marks
+            NSRange markRange = NSMakeRange(location, numberOfmark+1) ;
+            NSMutableDictionary *attrMark = [configuration.markStyle mutableCopy] ;
+            [attrMark setObject:[UIFont systemFontOfSize:0] forKey:NSFontAttributeName] ;
+            [attributedString addAttributes:attrMark range:markRange] ;
+        }
+            break;
+            
+        default:
+            break;
+    }
+    
+    return attributedString ;
+}
+
+- (NSMutableAttributedString *)addAttrOnEditState:(NSMutableAttributedString *)attributedString
+                                           config:(MDThemeConfiguration *)configuration {
+    
+    NSDictionary *resultDic = configuration.basicStyle ;
+    //    UIFont *paragraphFont = configuration.font ;
+    NSUInteger location = self.range.location ;
+//    NSUInteger length = self.range.length ;
+    
+    switch (self.type) {
+        case MarkdownSyntaxHeaders: {
+            NSString *prefix = [self prefixOfTitle] ;
             NSUInteger numberOfmark = prefix.length ;
             switch (numberOfmark) {
                 case 1: resultDic = @{NSFontAttributeName : [UIFont boldSystemFontOfSize:32]}; break;
@@ -56,24 +98,11 @@
             [attributedString addAttributes:configuration.markStyle range:markRange] ;
         }
             break;
-//        case MarkdownSyntaxLHeader: {
-//            resultDic = @{NSFontAttributeName : [UIFont boldSystemFontOfSize:32]} ;
-//            [attributedString addAttributes:resultDic range:self.range] ;
-//
-//            NSString *nail = [[self.str componentsSeparatedByString:@"\n"] lastObject] ;
-//            NSRange markRange = NSMakeRange(location + length - nail.length, nail.length) ;
-//            [attributedString addAttributes:configuration.markStyle range:markRange] ;
-//        }
             
         default:
             break;
     }
     
-    return attributedString ;
-}
-
-- (NSMutableAttributedString *)addAttrOnEditState:(NSMutableAttributedString *)attributedString
-                                           config:(MDThemeConfiguration *)configuration {
     return attributedString ;
 }
 
