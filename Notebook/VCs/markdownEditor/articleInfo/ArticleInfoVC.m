22091fdacbe88d46e7720ced4398b6cbb9991fe7
谢天宸 <xietianchen@shimo.im>
2019-05-14 17:16:15 +0800

    [fix] 文档信息里边，如果在暂存区里边的笔记，则位置显示暂存区，不留空白

--- Notebook/VCs/markdownEditor/articleInfo/ArticleInfoVC.m
+++ Notebook/VCs/markdownEditor/articleInfo/ArticleInfoVC.m
@@ -46,7 +46,7 @@
 
 - (void)bind {
     NoteBooks *book = [NoteBooks xt_findFirstWhere:XT_STR_FORMAT(@"icRecordName == '%@'",self.aNote.noteBookId)] ;
-    self.lbBookName.text = book.displayBookName ;
+    self.lbBookName.text = book.displayBookName ?: @"暂存区" ;
     self.lbCreateTime.text = [NSDate xt_getStrWithTick:self.aNote.createDateOnServer format:kTIME_STR_FORMAT_YYYY_MM_dd_HH_mm] ;
     self.lbUpdateTime.text = [NSDate xt_getStrWithTick:self.aNote.modifyDateOnServer format:kTIME_STR_FORMAT_YYYY_MM_dd_HH_mm] ;
     self.lbCountOfWord.text = @(self.parser.countForWord).stringValue ;
