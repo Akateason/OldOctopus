5e822781a4afd3402044b10984d9c0ba658b079e
谢天宸 <xietianchen@shimo.im>
2019-08-02 11:52:48 +0800

    b29 线上提交 008. 通过之后迭代 iap分支

--- Notebook/AppDelegate.h
+++ Notebook/AppDelegate.h
@@ -8,7 +8,7 @@
 #import <UIKit/UIKit.h>
 #import "LaunchingEvents.h"
 
-static const int k_Is_Internal_Testing = 1 ;  // 是否打开内测, 0默认关闭,  1打开内测
+static const int k_Is_Internal_Testing = 0 ;  // 是否打开内测, 0默认关闭,  1打开内测
 
 
 
